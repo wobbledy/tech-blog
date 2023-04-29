487673320d47a42921c973199e2d708276e26cba
Zach Leone <zjleone@gmail.com>
2023-04-29 14:41:06 -0700

    brought posts to frontend

---
+++ seeds/seed.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
