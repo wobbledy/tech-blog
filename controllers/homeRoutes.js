ebaa015488f4f4465b07e54899086dfa1f6d4cc5
Zach Leone <zjleone@gmail.com>
2023-04-29 15:43:33 -0700

    added signup page to separate login and sign up

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -83,11 +83,20 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('signup');
 });
 
 module.exports = router;
