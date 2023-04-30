c56a36df40d4f90cc81a44b77aa85ab2ce87b306
Zach Leone <zjleone@gmail.com>
2023-04-29 18:54:06 -0700

    added styling to the navbar and removed jass.cs

---
+++ public/js/post.js
@@ -0,0 +1,19 @@
+const delButtonHandler = async (event) => {
+    console.log(event.target);
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete post');
+        }
+    }
+};
+
+document
+    .querySelector('.post-delete')
+    .addEventListener('click', delButtonHandler);
