f1747c5c2128d6138c2b8bba3b9526b510a03fc5
Zach Leone <zjleone@gmail.com>
2023-04-29 19:35:53 -0700

    added styling to the dashboard handlebars page

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,8 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const title = document.querySelector('#post-name').value.trim();
-  const content = document.querySelector('#post-desc').value.trim();
+  const title = document.querySelector('#title').value.trim();
+  const content = document.querySelector('#content').value.trim();
 
   if (title && content) {
     const response = await fetch(`/api/posts`, {
